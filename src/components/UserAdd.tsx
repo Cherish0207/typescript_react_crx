ad763775856d8a627ee1c2c8401d013daf89323a
Cherish0207 <cheshire_1207@foxmail.com>
2021-06-07 15:31:22 +0800

    添加用户

--- src/components/UserAdd.tsx
+++ src/components/UserAdd.tsx
@@ -1,11 +1,47 @@
-import React, { Component } from "react";
-import { Layout, Menu } from "antd";
-import { Link, withRouter, RouteComponentProps, Route } from "react-router-dom";
+import React, { Component, useState } from "react";
+import { Form, Input, Button, message } from "antd";
+import { withRouter, RouteComponentProps, Route } from "react-router-dom";
+import { User, UserAddResponse } from "../typings/api";
+import request, { AxiosResponse } from "../api/request";
 type Props = RouteComponentProps;
-let { Sider, Content } = Layout;
-class UserAdd extends Component<Props> {
-  render() {
-    return <div>user add</div>;
-  }
+function UserAdd(props: Props) {
+  let [user, setUser] = useState<User>({} as User);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("user,", user);
+    request
+      .post<UserAddResponse>("/api/users", user)
+      .then((response: AxiosResponse<UserAddResponse>) => {
+        let { data, code } = response.data;
+        if (code === 0) {
+          props.history.push("/user/list");
+        } else {
+          message.error("添加用户失败");
+        }
+      });
+  };
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({
+      ...user,
+      username: event.target.value,
+    });
+  };
+  return (
+    <Form onFinish={handleSubmit}>
+      <Form.Item>
+        <Input
+          placeholder='用户名'
+          style={{ width: 120 }}
+          value={user.username}
+          onChange={handleNameChange}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type='primary' htmlType='submit'>
+          add
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
-export default withRouter(UserAdd);
+export default UserAdd;
--- src/typings/api.tsx
+++ src/typings/api.tsx
@@ -6,3 +6,8 @@ export interface UserListResponse {
   code: number;
   data: Array<User>;
 }
+
+export interface UserAddResponse {
+  code: number;
+  data: User;
+}
